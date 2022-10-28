0fa628cd310f1b0f426ce866baadee91865b8c82
Dave Samojlenko <dsamojlenko@gmail.com>
2022-10-28 20:35:37 +0000

    some logging n stuff

--- src/Engine/Engine.php
+++ src/Engine/Engine.php
@@ -91,38 +91,43 @@ class Engine
         }
     }
 
-    private function lookForFood()
+    private function lookForFood($spaces)
     {
-        $spaces = 1;
+        error_log("Looking for food " . $spaces);
 
         // Look for nearby food
         foreach ($this->board->food as $food) {
             // Check up
             if (in_array('up', $this->possibleMoves)) {
                 if ($this->me->head->y + $spaces == $food->y && $this->me->head->x == $food->x) {
+                    error_log("Found some food");
                     return 'up';
                 }
             }
             // Check down
             if (in_array('down', $this->possibleMoves)) {
                 if ($this->me->head->y - $spaces == $food->y && $this->me->head->x == $food->x) {
+                    error_log("Found some food");
                     return 'down';
                 }
             }
             // Check right
             if (in_array('right', $this->possibleMoves)) {
                 if ($this->me->head->x + $spaces == $food->x && $this->me->head->y == $food->y) {
+                    error_log("Found some food");
                     return 'right';
                 }
             }
             // Check left
             if (in_array('left', $this->possibleMoves)) {
                 if ($this->me->head->x - $spaces == $food->x && $this->me->head->y == $food->y) {
+                    error_log("Found some food");
                     return 'left';
                 }
             }
-            return;
         }
+      error_log("No food found");
+      return false;
     }
 
     public function getMove()
@@ -130,9 +135,12 @@ class Engine
         $this->avoidWalls();
         $this->avoidSnakes();
 
-        // Default random in case the below choices don't work out
-        $move = $this->possibleMoves[array_rand($this->possibleMoves)];
-
+        if (!$move = $this->lookForFood(1)) {
+          if (!$move = $this->lookForFood(2)) {
+            $move =  $this->possibleMoves[array_rand($this->possibleMoves)];
+          }
+        }
+      
         error_log("Moving: " . ($move ? $move : "Oh no, nowhere to go!"));
 
         return $move ? $move : "up";
