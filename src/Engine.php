31384489251aa5e92ffa690bc653b1ee0d34e16d
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-10-28 14:53:23 -0400

    refactor a bit

--- src/Engine.php
+++ src/Engine.php
@@ -6,124 +6,129 @@ namespace DaveSnake;
 
 class Engine
 {
-    private $data;
+    private object $data;
+    private object $me;
+    private object $board;
+    private array $possibleMoves;
 
     public function __construct($data)
     {
         $this->data = $data;
+        $this->board = $data->board;
+        $this->me = $data->you;
+        $this->me->tail = end($this->me->body);
+        $this->possibleMoves = ['up', 'down', 'left', 'right'];
     }
 
-    public function getMove()
+    private function avoidWalls()
     {
-        $board = $this->data->board;
-        $me = $this->data->you;
-
-        $possibleMoves = ['up', 'down', 'left', 'right'];
-
         // Check for walls
-        foreach($possibleMoves as $move) {
+        foreach($this->possibleMoves as $move) {
             // Check up
-            if ($me->head->y + 1 > $board->height-1) {
-                $possibleMoves = array_filter($possibleMoves, function($move) {
+            if ($this->me->head->y + 1 > $this->board->height-1) {
+                $this->possibleMoves = array_filter($this->possibleMoves, function($move) {
                     return $move !== "up";
                 });
             }
             // Check down
-            if ($me->head->y -1 < 0) {
-                $possibleMoves = array_filter($possibleMoves, function($move) {
+            if ($this->me->head->y -1 < 0) {
+                $this->possibleMoves = array_filter($this->possibleMoves, function($move) {
                     return $move !== "down";
                 });
             }
             // Check left
-            if ($me->head->x -1 < 0) {
-                $possibleMoves = array_filter($possibleMoves, function($move) {
+            if ($this->me->head->x -1 < 0) {
+                $this->possibleMoves = array_filter($this->possibleMoves, function($move) {
                     return $move !== "left";
                 });
             }
             // Check right
-            if ($me->head->x + 1 > $board->width-1) {
-                $possibleMoves = array_filter($possibleMoves, function($move) {
+            if ($this->me->head->x + 1 > $this->board->width-1) {
+                $this->possibleMoves = array_filter($this->possibleMoves, function($move) {
                     return $move !== "right";
                 });
             }
         }
+    }
 
-        $me->tail = end($me->body);
+    private function avoidSnakes()
+    {
         // Avoid snakes
-        foreach ($board->snakes as $snake) {
+        foreach ($this->board->snakes as $snake) {
             foreach($snake->body as $part) {
-                if($me->head->y + 1 == $part->y && $me->head->x == $part->x) {
-                    if(!($me->head->y + 1 == $me->tail->y && $me->head->x == $me->tail->x)) {
-                        $possibleMoves = array_filter($possibleMoves, function($move) {
+                if($this->me->head->y + 1 == $part->y && $this->me->head->x == $part->x) {
+                    if(!($this->me->head->y + 1 == $this->me->tail->y && $this->me->head->x == $this->me->tail->x)) {
+                        $this->possibleMoves = array_filter($this->possibleMoves, function($move) {
                             return $move !== "up";
                         });
                     }
                 }
-                if($me->head->y - 1 == $part->y && $me->head->x == $part->x) {
-                    if(!($me->head->y - 1 == $me->tail->y && $me->head->x == $me->tail->x)) {
-                        $possibleMoves = array_filter($possibleMoves, function($move) {
+                if($this->me->head->y - 1 == $part->y && $this->me->head->x == $part->x) {
+                    if(!($this->me->head->y - 1 == $this->me->tail->y && $this->me->head->x == $this->me->tail->x)) {
+                        $this->possibleMoves = array_filter($this->possibleMoves, function($move) {
                             return $move !== "down";
                         });
                     }
                 }
-                if($me->head->x + 1 == $part->x && $me->head->y == $part->y) {
-                    if(!($me->head->x + 1 == $me->tail->x && $me->head->y == $me->tail->y)) {
-                        $possibleMoves = array_filter($possibleMoves, function($move) {
+                if($this->me->head->x + 1 == $part->x && $this->me->head->y == $part->y) {
+                    if(!($this->me->head->x + 1 == $this->me->tail->x && $this->me->head->y == $this->me->tail->y)) {
+                        $this->possibleMoves = array_filter($this->possibleMoves, function($move) {
                             return $move !== "right";
                         });
                     }
                 }
-                if($me->head->x - 1 == $part->x && $me->head->y == $part->y) {
-                    if(!($me->head->x - 1 == $me->tail->x && $me->head->y == $me->tail->y)) {
-                        $possibleMoves = array_filter($possibleMoves, function($move) {
+                if($this->me->head->x - 1 == $part->x && $this->me->head->y == $part->y) {
+                    if(!($this->me->head->x - 1 == $this->me->tail->x && $this->me->head->y == $this->me->tail->y)) {
+                        $this->possibleMoves = array_filter($this->possibleMoves, function($move) {
                             return $move !== "left";
                         });
                     }
                 }
             }
         }
+    }
+
+    private function lookForFood()
+    {
+        $spaces = 1;
+
+        // Look for nearby food
+        foreach ($this->board->food as $food) {
+            // Check up
+            if (in_array('up', $this->possibleMoves)) {
+                if ($this->me->head->y + $spaces == $food->y && $this->me->head->x == $food->x) {
+                    return 'up';
+                }
+            }
+            // Check down
+            if (in_array('down', $this->possibleMoves)) {
+                if ($this->me->head->y - $spaces == $food->y && $this->me->head->x == $food->x) {
+                    return 'down';
+                }
+            }
+            // Check right
+            if (in_array('right', $this->possibleMoves)) {
+                if ($this->me->head->x + $spaces == $food->x && $this->me->head->y == $food->y) {
+                    return 'right';
+                }
+            }
+            // Check left
+            if (in_array('left', $this->possibleMoves)) {
+                if ($this->me->head->x - $spaces == $food->x && $this->me->head->y == $food->y) {
+                    return 'left';
+                }
+            }
+            return;
+        }
+    }
+
+    public function getMove()
+    {
+        $this->avoidWalls();
+        $this->avoidSnakes();
 
         // Default random in case the below choices don't work out
-        $move = $possibleMoves[array_rand($possibleMoves)];
-
-//        function getFoodCloseBy($possibleMoves, $board, $spaces) {
-//            // Look for nearby food
-//            foreach ($board->food as $food) {
-//                // Check up
-//                if (in_array('up', $possibleMoves)) {
-//                    if ($me->head->y + $spaces == $food->y && $me->head->x == $food->x) {
-//                        return 'up';
-//                    }
-//                }
-//                // Check down
-//                if (in_array('down', $possibleMoves)) {
-//                    if ($me->head->y - $spaces == $food->y && $me->head->x == $food->x) {
-//                        return 'down';
-//                    }
-//                }
-//                // Check right
-//                if (in_array('right', $possibleMoves)) {
-//                    if ($me->head->x + $spaces == $food->x && $me->head->y == $food->y) {
-//                        return 'right';
-//                    }
-//                }
-//                // Check left
-//                if (in_array('left', $possibleMoves)) {
-//                    if ($me->head->x - $spaces == $food->x && $me->head->y == $food->y) {
-//                        return 'left';
-//                    }
-//                }
-//                return;
-//            }
-//        }
-//
-//        if (!$move = getFoodCloseBy($possibleMoves, $board, 1)) {
-//            error_log("nothing 1 space away");
-//            if(!$move = getFoodCloseBy($possibleMoves, $board, 2)) {
-//                error_log("nothing 2 spaces away");
-//                $move = $possibleMoves[array_rand($possibleMoves)];
-//            }
-//        }
+        $move = $this->possibleMoves[array_rand($this->possibleMoves)];
 
         error_log("Moving: " . $move);
 
