6b054b7070dcadf47626977ac4c2d812b19937f0
Dave Samojlenko <dsamojlenko@gmail.com>
2022-10-28 23:58:26 -0400

    comments

--- src/Engine/AvoidSnakes.php
+++ src/Engine/AvoidSnakes.php
@@ -23,34 +23,33 @@ class AvoidSnakes
 
     public function getMoves($possibleMoves)
     {
-        // @TODO: don't worry about snake bums
         foreach ($this->board->snakes as $snake) {
             foreach($snake->body as $part) {
                 if($this->me->head->y + 1 == $part->y && $this->me->head->x == $part->x) {
-                    if(!($this->me->head->y + 1 == $this->tail->y && $this->me->head->x == $this->tail->x)) {
+                    if(!($this->me->head->y + 1 == $this->tail->y && $this->me->head->x == $this->tail->x)) { // Don't worry about the tail
                         $possibleMoves = array_filter($possibleMoves, function($move) {
-                            return $move !== "up";
+                            return $move !== "up"; // can't go up
                         });
                     }
                 }
                 if($this->me->head->y - 1 == $part->y && $this->me->head->x == $part->x) {
                     if(!($this->me->head->y - 1 == $this->tail->y && $this->me->head->x == $this->tail->x)) {
                         $possibleMoves = array_filter($possibleMoves, function($move) {
-                            return $move !== "down";
+                            return $move !== "down"; // can't go down
                         });
                     }
                 }
                 if($this->me->head->x + 1 == $part->x && $this->me->head->y == $part->y) {
                     if(!($this->me->head->x + 1 == $this->tail->x && $this->me->head->y == $this->tail->y)) {
                         $possibleMoves = array_filter($possibleMoves, function($move) {
-                            return $move !== "right";
+                            return $move !== "right"; // can't go right
                         });
                     }
                 }
                 if($this->me->head->x - 1 == $part->x && $this->me->head->y == $part->y) {
                     if(!($this->me->head->x - 1 == $this->tail->x && $this->me->head->y == $this->tail->y)) {
                         $possibleMoves = array_filter($possibleMoves, function($move) {
-                            return $move !== "left";
+                            return $move !== "left"; // can't go left
                         });
                     }
                 }
