e5cee9a0076f8b8160ea77b4957fd6ed864794aa
Dave Samojlenko <dsamojlenko@gmail.com>
2022-10-29 11:37:45 -0400

    formatting

--- src/Engine/AvoidWalls.php
+++ src/Engine/AvoidWalls.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace DaveSnake\Engine;
 
-use DaveSnake\Models\Board;
 use DaveSnake\Models\BattleSnake;
+use DaveSnake\Models\Board;
 use DaveSnake\Models\Move;
 
 class AvoidWalls
@@ -21,33 +21,33 @@ class AvoidWalls
 
     private function eliminateMove($possibleMoves, $eliminated): array
     {
-        return array_filter($possibleMoves, function($move) use ($eliminated) {
+        return array_filter($possibleMoves, function ($move) use ($eliminated) {
             return $move !== $eliminated;
         });
     }
 
-  public function getMoves($possibleMoves)
-  {
-      // Check for walls
-      foreach($possibleMoves as $move) {
-          // Check up
-          if ($this->me->head->y + 1 > $this->board->height-1) {
-              $possibleMoves = $this->eliminateMove($possibleMoves, Move::$UP);
-          }
-          // Check down
-          if ($this->me->head->y -1 < 0) {
-              $possibleMoves = $this->eliminateMove($possibleMoves, Move::$DOWN);
-          }
-          // Check left
-          if ($this->me->head->x -1 < 0) {
-              $possibleMoves = $this->eliminateMove($possibleMoves, Move::$LEFT);
-          }
-          // Check right
-          if ($this->me->head->x + 1 > $this->board->width-1) {
-              $possibleMoves = $this->eliminateMove($possibleMoves, Move::$RIGHT);
-          }
-      }
-      
-      return $possibleMoves;
-  }
+    public function getMoves($possibleMoves)
+    {
+        // Check for walls
+        foreach ($possibleMoves as $move) {
+            // Check up
+            if ($this->me->head->y + 1 > $this->board->height - 1) {
+                $possibleMoves = $this->eliminateMove($possibleMoves, Move::$UP);
+            }
+            // Check down
+            if ($this->me->head->y - 1 < 0) {
+                $possibleMoves = $this->eliminateMove($possibleMoves, Move::$DOWN);
+            }
+            // Check left
+            if ($this->me->head->x - 1 < 0) {
+                $possibleMoves = $this->eliminateMove($possibleMoves, Move::$LEFT);
+            }
+            // Check right
+            if ($this->me->head->x + 1 > $this->board->width - 1) {
+                $possibleMoves = $this->eliminateMove($possibleMoves, Move::$RIGHT);
+            }
+        }
+
+        return $possibleMoves;
+    }
 }
