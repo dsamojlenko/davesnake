03aeee7cf13d88b454daa29cdf7321aa5bcdc945
Dave Samojlenko <dsamojlenko@gmail.com>
2022-10-31 21:35:36 -0400

    Avoids one space traps

--- src/Engine/AvoidTraps.php
+++ src/Engine/AvoidTraps.php
@@ -9,39 +9,62 @@ use DaveSnake\Models\Coordinates;
 class AvoidTraps extends AvoidanceBaseClass implements AvoidanceInterface
 {
 
+    public function checkNextMove(Coordinates $target)
+    {
+        // get cells adjacent to target
+        $adjacentCells = array_map(function($cell) {
+            return $this->board->getLocationIdFromCoordinates($cell);
+        }, $this->board->getAdjacentCells($target));
+
+        error_log("[AvoidTraps] adjacentCells: " . print_r($adjacentCells, true));
+
+        // get all hazards
+        $allHazards = array_map(function($hazard) {
+            return $this->board->getLocationIdFromCoordinates(new Coordinates($hazard));
+        }, $this->getAllHazards());
+
+        error_log("[AvoidTraps] allHazards: " . print_r($allHazards, true));
+
+        $potentialMoves = [];
+        foreach($adjacentCells as $move => $position) {
+            if (!in_array($position, $allHazards)) {
+                array_push($potentialMoves, $move);
+            }
+        }
+        return $potentialMoves;
+    }
+
     public function filterMoves($possibleMoves): array
     {
         error_log("[AvoidTraps] Looking ahead to check for exits");
+
         foreach($possibleMoves as $move) {
+            error_log("[AvoidTraps] Checking " . $move);
+
+            // select a target
             $target = $this->getMoveTargetCoordinates($move, 1);
 
-            $allHazards = [];
-            foreach($this->board->snakes as $snake) {
-                $allHazards = array_merge($allHazards, $snake->body);
-            }
+            $potentialMoves = $this->checkNextMove($target);
 
-            $adjacentCells = $this->board->getAdjacentCells($target);
-            $adjacentCells = array_values(array_map(function($coordinate) {
-                return (object)[
-                    "x" => $coordinate->x,
-                    "y" => $coordinate->y,
-                ];
-            }, $adjacentCells));
-
-            // $boardHazards = [...$this->board->hazards];
-            $allHazards = array_merge($allHazards, $this->board->hazards);
-
-            $intersection = array_uintersect($allHazards, $adjacentCells, function($a, $b) {
-                return strcmp(spl_object_hash($a), spl_object_hash($b));
-            });
-
-            error_log("[AvoidTraps] intersection: " . print_r($intersection, true));
-
-            if(!empty($intersection)) {
+            if(!$potentialMoves) {
                 $possibleMoves = $this->eliminateMove($possibleMoves, $move);
             }
+
+        }
+        error_log("[AvoidTraps] Remaining moves: " . print_r($possibleMoves, true));
+        return $possibleMoves;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAllHazards(): array
+    {
+        $allHazards = [];
+        foreach ($this->board->snakes as $snake) {
+            $allHazards = array_merge($allHazards, $snake->body);
         }
 
-        return $possibleMoves;
+        return array_merge($allHazards, $this->board->hazards);
     }
 }
